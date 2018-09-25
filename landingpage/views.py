de7f635d6d5945f1925d7dae06406124897ccb2a
Matt Anderson <mtander3@ncsu.edu>
2018-09-25 16:56:26 -0400

    landing page updates

--- landingpage/views.py
+++ landingpage/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from blog.models import Post
-
+from . import forms
 
 #Landing page
 def home(request):
@@ -13,5 +13,13 @@ def home(request):
 
 def test(request):
 
-	posts = Post.objects.order_by('-published_date')[:3]
-	return render(request,'landingpage/test.html',{'posts':posts})
+	form = forms.ContactForm
+
+	if request.method == "POST":
+		form = forms.ContactForm(request.POST)
+
+		if form.is_valid():
+			print(form.cleaned_data['name'])
+			print(form.cleaned_data['email'])
+			print(form.cleaned_data['text'])
+	return render(request,'landingpage/test.html',{'form':form})
